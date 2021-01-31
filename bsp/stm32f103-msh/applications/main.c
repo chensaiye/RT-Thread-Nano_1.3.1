9f988a838935cb478c7ef76775f2b79561d59714
chensaiye <chensaiye@163.com>
2021-01-31 10:57:07 +0800

    20210131 modify PD test use timer2

--- bsp/stm32f103-msh/applications/main.c
+++ bsp/stm32f103-msh/applications/main.c
@@ -30,12 +30,12 @@
 
 /* 定时器的控制块 */
 static rt_timer_t timer1;
-//static rt_timer_t timer2;
+static rt_timer_t timer2;
 
 extern uint8_t Channels_RIR[CHANNEL_RIR_MAX];
 
 static void MX_GPIO_Init(void);
-static void MX_EXTI_GPIO_Init(void);
+//static void MX_EXTI_GPIO_Init(void);
 
 extern void Panel_Init(void);
 extern void backup_data(void);
@@ -55,46 +55,46 @@ void Error_Handler(void)
 
   /* USER CODE END Error_Handler_Debug */
 }
-void PowDown_Test_Init(void)
-{
+//void PowDown_Test_Init(void)
+//{
 
-	PWR_PVDTypeDef PWR_PVD_INIT_Struct={0};
-	/* Enable PWR and BKP clock */
-	__HAL_RCC_BKP_CLK_ENABLE();
-	__HAL_RCC_PWR_CLK_ENABLE();
-	
-	/* Configure EXTI Line to generate an interrupt on falling edge */
-		
-	/* NVIC configuration */
-	/* PVD_IRQn interrupt configuration */
-	HAL_NVIC_SetPriority(PVD_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(PVD_IRQn);
-	
-	PWR_PVD_INIT_Struct.Mode = PWR_PVD_MODE_IT_RISING;//why is rising???
-	PWR_PVD_INIT_Struct.PVDLevel =  PWR_PVDLEVEL_7;
-	HAL_PWR_ConfigPVD(&PWR_PVD_INIT_Struct);
-	HAL_PWR_EnablePVD();
-				 
-}
+//	PWR_PVDTypeDef PWR_PVD_INIT_Struct={0};
+//	/* Enable PWR and BKP clock */
+//	__HAL_RCC_BKP_CLK_ENABLE();
+//	__HAL_RCC_PWR_CLK_ENABLE();
+//	
+//	/* Configure EXTI Line to generate an interrupt on falling edge */
+//		
+//	/* NVIC configuration */
+//	/* PVD_IRQn interrupt configuration */
+//	HAL_NVIC_SetPriority(PVD_IRQn, 0, 0);
+//  HAL_NVIC_EnableIRQ(PVD_IRQn);
+//	
+//	PWR_PVD_INIT_Struct.Mode = PWR_PVD_MODE_IT_RISING;//why is rising???
+//	PWR_PVD_INIT_Struct.PVDLevel =  PWR_PVDLEVEL_7;
+//	HAL_PWR_ConfigPVD(&PWR_PVD_INIT_Struct);
+//	HAL_PWR_EnablePVD();
+//				 
+//}
 
 /**
   * @brief This function handles PVD interrupt through EXTI line 16.
   */
-void PVD_IRQHandler(void)
-{
-  /* USER CODE BEGIN PVD_IRQn 0 */
-	HAL_GPIO_WritePin(GP1_POW_GPIO_Port, GP1_POW_Pin, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(QJ_POW_GPIO_Port, QJ_POW_Pin, GPIO_PIN_RESET);
-	//HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_SET);
-	rt_enter_critical();//调度器上锁
-	backup_data();
-  /* USER CODE END PVD_IRQn 0 */
-  HAL_PWR_PVD_IRQHandler();
-  /* USER CODE BEGIN PVD_IRQn 1 */
-	//HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET);
-	rt_exit_critical();//调度器解锁	
-  /* USER CODE END PVD_IRQn 1 */
-}
+//void PVD_IRQHandler(void)
+//{
+//  /* USER CODE BEGIN PVD_IRQn 0 */
+//	HAL_GPIO_WritePin(GP1_POW_GPIO_Port, GP1_POW_Pin, GPIO_PIN_RESET);
+//	HAL_GPIO_WritePin(QJ_POW_GPIO_Port, QJ_POW_Pin, GPIO_PIN_RESET);
+//	//HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_SET);
+//	rt_enter_critical();//调度器上锁
+//	backup_data();
+//  /* USER CODE END PVD_IRQn 0 */
+//  HAL_PWR_PVD_IRQHandler();
+//  /* USER CODE BEGIN PVD_IRQn 1 */
+//	//HAL_GPIO_WritePin(LED0_GPIO_Port, LED0_Pin, GPIO_PIN_RESET);
+//	rt_exit_critical();//调度器解锁	
+//  /* USER CODE END PVD_IRQn 1 */
+//}
 
 
 /* 定时器1超时函数 */
@@ -103,17 +103,36 @@ static void time1_out(void *parameter)
  // HAL_GPIO_TogglePin(RUN_GPIO_PORT, RUN_PIN);
 	Led_Blink();
 }
-
+static uint8_t PD_Count= 0;
 ///* 定时器1超时函数 */
-//static void time2_out(void *parameter)
-//{
+static void time2_out(void *parameter)
+{
+	if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_2)==GPIO_PIN_SET)
+	{
+		PD_Count++;
+		if(PD_Count>= 2)
+		{
+			rt_enter_critical();//调度器上锁
+			HAL_GPIO_WritePin(GP1_POW_GPIO_Port, GP1_POW_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(QJ_POW_GPIO_Port, QJ_POW_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+			backup_data();
+			rt_exit_critical();//调度器解锁
+			rt_timer_stop(timer2);
+		}
+		else
+			return;
+	}
+	else
+		PD_Count = 0;
+	
 //		uint16_t distance;
 //		distance = TOF10120_Read_Scan();
 //		if((distance>450) && Channels_RIR[6])
 //			bt_rir_7(BUTTON_LONG_RELEASE);
 //		if((distance<=450) && Channels_RIR[6]==0)
 //			bt_rir_7(BUTTON_LONG_PRESSED);
-//}
+}
 
 static void times_init(void)
 {
@@ -126,12 +145,12 @@ static void times_init(void)
         rt_timer_start(timer1);
 		
 //		/* 创建定时器2  周期定时器 */
-//    timer2 = rt_timer_create("timer2", time2_out,
-//                             RT_NULL, 200,
-//                             RT_TIMER_FLAG_PERIODIC);
-//    /* 启动定时器1 */
-//    if (timer2 != RT_NULL)
-//        rt_timer_start(timer2);
+    timer2 = rt_timer_create("timer2", time2_out,
+                             RT_NULL, 50,
+                             RT_TIMER_FLAG_PERIODIC);
+    /* 启动定时器2 */
+    if (timer2 != RT_NULL)
+        rt_timer_start(timer2);
 		
 }
 	
@@ -150,7 +169,7 @@ int main(void)
 		//thread_usart3_init();
 		
 		rt_thread_mdelay(200);
-		MX_EXTI_GPIO_Init();
+		//MX_EXTI_GPIO_Init();
 		//PowDown_Test_Init();
 	
 		times_init();
@@ -195,50 +214,50 @@ static void MX_GPIO_Init(void)
   * @param None
   * @retval None
   */
-static void MX_EXTI_GPIO_Init(void)
-{	
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
+//static void MX_EXTI_GPIO_Init(void)
+//{	
+//	GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-	
-/*Configure GPIO pin : PB2 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-	
- /* EXTI interrupt init*/
+//  /* GPIO Ports Clock Enable */
+//  __HAL_RCC_GPIOB_CLK_ENABLE();
+//	
+///*Configure GPIO pin : PB2 */
+//  GPIO_InitStruct.Pin = GPIO_PIN_2;
+//  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+//  GPIO_InitStruct.Pull = GPIO_NOPULL;
+//  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+//	
+// /* EXTI interrupt init*/
 
-	HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
-}
+//	HAL_NVIC_SetPriority(EXTI2_IRQn, 0, 0);
+//  HAL_NVIC_EnableIRQ(EXTI2_IRQn);
+//}
 
 /**
   * @brief This function handles EXTI line2 interrupt.
   */
-void EXTI2_IRQHandler(void)
-{
-	
-  /* USER CODE BEGIN EXTI2_IRQn 0 */
-	if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_2)==GPIO_PIN_SET)
-	{
-		rt_enter_critical();//调度器上锁
-		HAL_GPIO_WritePin(GP1_POW_GPIO_Port, GP1_POW_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(QJ_POW_GPIO_Port, QJ_POW_Pin, GPIO_PIN_RESET);
-		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
-		/* USER CODE END EXTI2_IRQn 0 */
-		HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
-		/* USER CODE BEGIN EXTI2_IRQn 1 */
-		//rt_thread_mdelay(10);
-		backup_data();
-		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
-		rt_exit_critical();//调度器解锁
-		/* USER CODE END EXTI2_IRQn 1 */
-	}
-	else
-		HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
-}
+//void EXTI2_IRQHandler(void)
+//{
+//	
+//  /* USER CODE BEGIN EXTI2_IRQn 0 */
+//	if(HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_2)==GPIO_PIN_SET)
+//	{
+//		rt_enter_critical();//调度器上锁
+//		HAL_GPIO_WritePin(GP1_POW_GPIO_Port, GP1_POW_Pin, GPIO_PIN_RESET);
+//		HAL_GPIO_WritePin(QJ_POW_GPIO_Port, QJ_POW_Pin, GPIO_PIN_RESET);
+//		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET);
+//		/* USER CODE END EXTI2_IRQn 0 */
+//		HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
+//		/* USER CODE BEGIN EXTI2_IRQn 1 */
+//		//rt_thread_mdelay(10);
+//		backup_data();
+//		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET);
+//		rt_exit_critical();//调度器解锁
+//		/* USER CODE END EXTI2_IRQn 1 */
+//	}
+//	else
+//		HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_2);
+//}
 
 /**
  * This function use to test some drive demo
